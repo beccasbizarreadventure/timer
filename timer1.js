42e82288aab3292913e93820804be7a2ff7c33e4
Rebecca <rebecca90469@gmail.com>
2024-02-11 13:39:21 -0500

    Fixed error throw

--- timer1.js
+++ timer1.js
@@ -3,36 +3,37 @@ const numberCompare = function(a, b) {
 };
 
 const timer = function(timedArray) {
+
+  if (timedArray.length === 0) {
+    throw new Error("Not a valid input"); // throws an error if no numbers inputted into the array
+  }
+
   timedArray.join();
   timedArray.sort();
-  const numberOrder = timedArray.sort(numberCompare);
-
-  // if (numberOrder.length === 0 ) {
-  //   throw new Error('No valid inputs given')
-  // };
+  const numberOrder = timedArray.sort(numberCompare); // makes sure '10' comes after '2'
 
   for (let i = 0; i < numberOrder.length; i++) {
-    if (typeof numberOrder[i] !== "number") {
+
+    if (typeof numberOrder[i] !== "number") { // skips any non number characters
       continue;
     }
-    if (numberOrder[i] < 0) {
+    if (numberOrder[i] < 0) { // skips any negative numbers
       continue;
     }
     setTimeout(() => {
-      process.stdout.write('.');
+      process.stdout.write('\x07');
     }, (i * 1000));
   }
 
-//   setTimeout(() => {
-//     console.log('\n');
-//   }, (numberOrder.length - 1) * 50);
 };
 
-const testArray = [4, 5, 3, 8];
-timer(testArray);
+// SAMPLE TEST CASES //
+
+// const testArray = [4, 5, 3, 8];
+// timer(testArray);
 // const testArray1 = ["a", "r", 4, 7, 3];
 // timer(testArray1);
-// const testArray2 = [-1 ,-2 , 6, 7, 8];
+// const testArray2 = [-1 ,-2 , 6, 7, 10];
 // timer(testArray2);
 // const testArray3 = [];
 // timer(testArray3);
