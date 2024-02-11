f2aa0169dd755c70b9787c9a5243a0ad7c4565db
Rebecca <rebecca90469@gmail.com>
2024-02-11 13:26:40 -0500

    Added a few more rrors cases, need to fix error when no numbers given to timer

--- timer1.js
+++ timer1.js
@@ -6,15 +6,33 @@ const timer = function(timedArray) {
   timedArray.join();
   timedArray.sort();
   const numberOrder = timedArray.sort(numberCompare);
-  if (numberOrder.length === 0 ) {
-    throw new Error('No valid inputs given')
-  };
+
+  // if (numberOrder.length === 0 ) {
+  //   throw new Error('No valid inputs given')
+  // };
+
   for (let i = 0; i < numberOrder.length; i++) {
+    if (typeof numberOrder[i] !== "number") {
+      continue;
+    }
+    if (numberOrder[i] < 0) {
+      continue;
+    }
     setTimeout(() => {
-      process.stdout.write('\x07');
+      process.stdout.write('.');
     }, (i * 1000));
   }
+
+//   setTimeout(() => {
+//     console.log('\n');
+//   }, (numberOrder.length - 1) * 50);
 };
 
-const testArray = [];
+const testArray = [4, 5, 3, 8];
 timer(testArray);
+// const testArray1 = ["a", "r", 4, 7, 3];
+// timer(testArray1);
+// const testArray2 = [-1 ,-2 , 6, 7, 8];
+// timer(testArray2);
+// const testArray3 = [];
+// timer(testArray3);
