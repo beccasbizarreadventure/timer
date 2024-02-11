5f0edd830a84c9d9c7b657f9055cf5fc729353b2
Rebecca <rebecca90469@gmail.com>
2024-02-11 13:05:54 -0500

    Added error for when no time inputted

--- timer1.js
+++ timer1.js
@@ -6,13 +6,15 @@ const timer = function(timedArray) {
   timedArray.join();
   timedArray.sort();
   const numberOrder = timedArray.sort(numberCompare);
+  if (numberOrder.length === 0 ) {
+    throw new Error('No valid inputs given')
+  };
   for (let i = 0; i < numberOrder.length; i++) {
     setTimeout(() => {
       process.stdout.write('\x07');
     }, (i * 1000));
   }
-}
+};
 
-
-const testArray = [10, 3, 5, 15, 9];
+const testArray = [];
 timer(testArray);
